41032ea26544c79580c440cf96c33f6676ea3a26
scrimpys <scrimpy.sailor@gmail.com>
2022-03-23 10:40:22 +0800

    adjust field size for filters in all pages and change submit button on regions_main_form.html

--- source/filters.py
+++ source/filters.py
@@ -13,11 +13,11 @@ STATE_CHOICES = (('RUN COMPLETED', 'Finished'), ('STARTED', 'Started'), ('ERROR'
 
 class CameraFilter(FilterSet):
     camera_name = CharFilter(lookup_expr='icontains',
-                             widget=TextInput(attrs={'size': '13'}))
+                             widget=TextInput(attrs={'size': '15'}), label="Name contains")
     url = CharFilter(lookup_expr='icontains',
-                     widget=TextInput(attrs={'size': '13'}))
+                     widget=TextInput(attrs={'size': '14'}), label="URL contains")
     camera_location = CharFilter(lookup_expr='icontains',
-                                 widget=TextInput(attrs={'size': '13'}))
+                                 widget=TextInput(attrs={'size': '18'}), label="Location contains")
     matching_threshold = NumberFilter(widget=NumberInput(attrs={'style': 'width:23ch'}))
     last_check_date = DateRangeFilter()
 
@@ -27,17 +27,17 @@ class CameraFilter(FilterSet):
 
 
 class LogFilter(FilterSet):
-    matching_score = RangeFilter(widget=RangeWidget(attrs={'size': '7'}))
-    focus_value = RangeFilter(widget=RangeWidget(attrs={'size': '7'}))
+    matching_score = RangeFilter(widget=RangeWidget(attrs={'size': '12'}), label="Match")
+    focus_value = RangeFilter(widget=RangeWidget(attrs={'size': '12'}), label="Focus")
     action = ChoiceFilter(choices=LOG_RESULT_CHOICES)
     creation_date = DateRangeFilter()
-    camera_name = CharFilter(field_name='url__camera_name', lookup_expr='icontains', label="Camera name contains",
-                             widget=TextInput(attrs={'size': '13'}))
-    camera_number = CharFilter(field_name='url__camera_number', lookup_expr='icontains', label="Camera number contains",
-                               widget=TextInput(attrs={'size': '13'}))
+    camera_name = CharFilter(field_name='url__camera_name', lookup_expr='icontains', label="Name contains",
+                             widget=TextInput(attrs={'size': '15'}))
+    camera_number = CharFilter(field_name='url__camera_number', lookup_expr='icontains', label="Number contains",
+                               widget=TextInput(attrs={'size': '18'}))
     camera_location = CharFilter(field_name='url__camera_location', lookup_expr='icontains',
-                                 label="Camera location contains",
-                                 widget=TextInput(attrs={'size': '13'}))
+                                 label="Location contains",
+                                 widget=TextInput(attrs={'size': '18'}))
 
     class Meta:
         model = LogImage
@@ -48,7 +48,7 @@ class LogFilter(FilterSet):
 class EngineStateFilter(FilterSet):
     state = ChoiceFilter(choices=STATE_CHOICES)
     state_timestamp = DateRangeFilter()
-    number_failed_images = RangeFilter(widget=RangeWidget(attrs={'size': '7'},), label="Number of fails")
+    number_failed_images = RangeFilter(widget=RangeWidget(attrs={'size': '7'},), label="Fails")
 
     class Meta:
         model = EngineState
