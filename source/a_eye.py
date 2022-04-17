e833192c7528e599cc482f051e724b5e5e7d8b39
scrimpys <scrimpy.sailor@gmail.com>
2022-04-17 15:20:15 +0800

    adjust window size from 5 to 48 to provide better score when images differ.  Larger window has more chance of seeing more changes especially with light or dark walls etc

--- source/a_eye.py
+++ source/a_eye.py
@@ -6,30 +6,29 @@ import cv2
 
 
 def movement(img1, img2):
-    c1 = (0.01 * 255)**2
-    c2 = (0.03 * 255)**2
+    c1 = (0.01 * 255) ** 2
+    c2 = (0.03 * 255) ** 2
 
     img1 = img1.astype(np.float64)
     img2 = img2.astype(np.float64)
     kernel = cv2.getGaussianKernel(11, 1.5)
     window = np.outer(kernel, kernel.transpose())
 
-    mu1 = cv2.filter2D(img1, -1, window)[5:-5, 5:-5]  # valid
-    mu2 = cv2.filter2D(img2, -1, window)[5:-5, 5:-5]
-    mu1_sq = mu1**2
-    mu2_sq = mu2**2
+    mu1 = cv2.filter2D(img1, -1, window)[48:-48, 48:-48]  # valid
+    mu2 = cv2.filter2D(img2, -1, window)[48:-48, 48:-48]
+    mu1_sq = mu1 ** 2
+    mu2_sq = mu2 ** 2
     mu1_mu2 = mu1 * mu2
-    sigma1_sq = cv2.filter2D(img1**2, -1, window)[5:-5, 5:-5] - mu1_sq
-    sigma2_sq = cv2.filter2D(img2**2, -1, window)[5:-5, 5:-5] - mu2_sq
-    sigma12 = cv2.filter2D(img1 * img2, -1, window)[5:-5, 5:-5] - mu1_mu2
+    sigma1_sq = cv2.filter2D(img1 ** 2, -1, window)[48:-48, 48:-48] - mu1_sq
+    sigma2_sq = cv2.filter2D(img2 ** 2, -1, window)[48:-48, 48:-48] - mu2_sq
+    sigma12 = cv2.filter2D(img1 * img2, -1, window)[48:-48, 48:-48] - mu1_mu2
 
     movement_map = ((2 * mu1_mu2 + c1) * (2 * sigma12 + c2)) / ((mu1_sq + mu2_sq + c1) *
-                                                            (sigma1_sq + sigma2_sq + c2))
+                                                                (sigma1_sq + sigma2_sq + c2))
     return movement_map.mean()
 
 
 def calculate_movement(img1, img2):
-
     if not img1.shape == img2.shape:
         raise ValueError('Input images must have the same dimensions.')
     if img1.ndim == 2:
