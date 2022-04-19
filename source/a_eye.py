acac1bb6ad8a35430ba2ece9c5e825a05e5134db
scrimpys <scrimpy.sailor@gmail.com>
2022-04-19 12:25:46 +0800

    adjust window size from 48 to 32 to provide better score when images differ.  Larger window has more chance of seeing more changes especially with light or dark walls etc

--- source/a_eye.py
+++ source/a_eye.py
@@ -14,14 +14,14 @@ def movement(img1, img2):
     kernel = cv2.getGaussianKernel(11, 1.5)
     window = np.outer(kernel, kernel.transpose())
 
-    mu1 = cv2.filter2D(img1, -1, window)[48:-48, 48:-48]  # valid
-    mu2 = cv2.filter2D(img2, -1, window)[48:-48, 48:-48]
+    mu1 = cv2.filter2D(img1, -1, window)[32:-32, 32:-32]  # valid
+    mu2 = cv2.filter2D(img2, -1, window)[32:-32, 32:-32]
     mu1_sq = mu1 ** 2
     mu2_sq = mu2 ** 2
     mu1_mu2 = mu1 * mu2
-    sigma1_sq = cv2.filter2D(img1 ** 2, -1, window)[48:-48, 48:-48] - mu1_sq
-    sigma2_sq = cv2.filter2D(img2 ** 2, -1, window)[48:-48, 48:-48] - mu2_sq
-    sigma12 = cv2.filter2D(img1 * img2, -1, window)[48:-48, 48:-48] - mu1_mu2
+    sigma1_sq = cv2.filter2D(img1 ** 2, -1, window)[32:-32, 32:-32] - mu1_sq
+    sigma2_sq = cv2.filter2D(img2 ** 2, -1, window)[32:-32, 32:-32] - mu2_sq
+    sigma12 = cv2.filter2D(img1 * img2, -1, window)[32:-32, 32:-32] - mu1_mu2
 
     movement_map = ((2 * mu1_mu2 + c1) * (2 * sigma12 + c2)) / ((mu1_sq + mu2_sq + c1) *
                                                                 (sigma1_sq + sigma2_sq + c2))
