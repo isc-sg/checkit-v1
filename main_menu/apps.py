995de7785fd20f820aa322762b9c11e4f86a9014
scrimpys <scrimpy.sailor@gmail.com>
2024-01-01 09:18:08 +1100

    start logging at boot

--- main_menu/apps.py
+++ main_menu/apps.py
@@ -1,7 +1,19 @@
+import logging
+from logging.handlers import RotatingFileHandler
 from django.apps import AppConfig
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s [%(lineno)d] \t - '
+                                               '%(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',
+                    handlers=[RotatingFileHandler('/home/checkit/camera_checker/logs/checkit.log',
+                                                  maxBytes=10000000, backupCount=10)])
 
 class MainMenuConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main_menu'
     verbose_name = "Checkit"
+
+    def ready(self):
+        pass  # startup code here
+        logging.info("Starting Checkit")
+        # insert license check code here.
+        # for now lets check adm but later lets check dongle.
