2d6506d990f2a5d23f16b72f4e4b03a481037a7e
scrimpys <scrimpy.sailor@gmail.com>
2024-02-21 01:27:53 +1100

    was getting a warning for having a / in camera_api URL.  This started after adding APPEND_SLASH = True to fix password URL access.  Fixed with ^/ in front.

--- main_menu/urls.py
+++ main_menu/urls.py
@@ -13,7 +13,7 @@ from main_menu import views
 router = routers.DefaultRouter()
 # router.register(r'users', views.UserViewSet)
 # router.register(r'groups', views.GroupViewSet)
-router.register(r'cameras_api', views.CameraViewSet)
+router.register(r'^/cameras_api', views.CameraViewSet)
 # router.register(r'index', views.index)
 
 urlpatterns = [
