e50e4a3e6765696d0fa5b4654a0fb554c3eb000a
scrimpys <scrimpy.sailor@gmail.com>
2024-11-19 06:34:53 +0800

    ignore

--- setup.py
+++ setup.py
@@ -1,24 +1,26 @@
-from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup
 from Cython.Build import cythonize
 fileSet = set()
-#fileSet.add("main_menu/a_eye.py")
-#fileSet.add("main_menu/admin.py")
-#fileSet.add("main_menu/apps.py")
-#fileSet.add("main_menu/dris.py")
-#fileSet.add("main_menu/filters.py")
-#fileSet.add("main_menu/forms.py")
-#fileSet.add("main_menu/models.py")
-#fileSet.add("main_menu/resources.py")
-#fileSet.add("main_menu/select_region.py")
-#fileSet.add("main_menu/serializers.py")
+fileSet.add("main_menu/a_eye.py")
+fileSet.add("main_menu/admin.py")
+fileSet.add("main_menu/apps.py")
+fileSet.add("main_menu/dris.py")
+fileSet.add("main_menu/filters.py")
+fileSet.add("main_menu/forms.py")
+fileSet.add("main_menu/models.py")
+fileSet.add("main_menu/resources.py")
+fileSet.add("main_menu/select_region.py")
+fileSet.add("main_menu/serializers.py")
 fileSet.add("main_menu/tasks.py")
-#fileSet.add("main_menu/tables.py")
+fileSet.add("main_menu/tables.py")
 fileSet.add("main_menu/urls.py")
 fileSet.add("main_menu/views.py")
 
 setup(description='Checkit Application',
       author='Sam Corbo',
       author_email='sam.corbo@isc.sg',
-      ext_modules=cythonize(fileSet, language_level=3)
+      ext_modules=cythonize(fileSet, language_level=3),
+      define_macros=[("VERSION", '"1.3.4"')]
 )
 
