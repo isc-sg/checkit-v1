2107a69938ea836fce3edb98909c9224570eab85
scrimpys <scrimpy.sailor@gmail.com>
2024-11-19 06:43:07 +0800

    umask change so that we have 664 for file creation

--- camera_checker/wsgi.py
+++ camera_checker/wsgi.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 
 import os
 
+os.umask(0o002)
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'camera_checker.settings')
